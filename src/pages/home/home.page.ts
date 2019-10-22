f49164dcbf7bd7e8e1c14c77441ea5cd6848e1a5
nicoLepaj <nicolas.lepajolec@gmail.com>
2019-10-22 13:43:03 +0200

    scroll event home page

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,105 +1,113 @@
-import { Component, ViewChild, HostListener } from "@angular/core";
+import { Component, ViewChild, HostListener } from '@angular/core';
 
-import { UserDataService } from "src/services/user-data.service";
+import { UserDataService } from 'src/services/user-data.service';
 
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import { Router } from "@angular/router";
-import { RecipeDataService } from "src/services/recipe-data.service";
-import { IonSlides } from "@ionic/angular";
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { Router } from '@angular/router';
+import { RecipeDataService } from 'src/services/recipe-data.service';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  @ViewChild("slides", { static: false }) slides: IonSlides;
 
-  @HostListener("window:click", ["$event"])
-  clickout() {
-    this.closeDropdown();
-  }
+	constructor(
+		public userService: UserDataService,
+		public router: Router,
+		public recipeService: RecipeDataService
+	) {
+		this.onAuthChange();
+		this.getRecipes();
+	}
+	@ViewChild('slides', { static: false }) slides: IonSlides;
 
-  @HostListener("window:scroll", ["$event"])
+	user: any;
+	allRecipes: any = [];
+	controlMenu: boolean;
+	showMenu: boolean;
+
+	slideOpts = {
+		slidesPerView: 1,
+		freeMode: false,
+		coverflowEffect: {
+			rotate: 50,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true
+		}
+	};
+
+	@HostListener('window:click', ['$event'])
+	clickout() {
+		this.closeDropdown();
+	}
+
+	/* @HostListener("window:scroll", ["$event"])
    onWindowsScroll() {
     console.log("scroll works");
-  }
+  } */
 
-  user: any;
-  allRecipes: any = [];
-  controlMenu: boolean;
-  showMenu: boolean;
-
-  constructor(
-    public userService: UserDataService,
-    public router: Router,
-    public recipeService: RecipeDataService
-  ) {
-    this.onAuthChange();
-    this.getRecipes();
-  }
-
-  openDropdown(ev) {
-    this.showMenu = !this.showMenu;
-    this.controlMenu = true;
-
-    if (!this.showMenu) {
-      ev.stopPropagation();
-      this.controlMenu = false;
+	onScroll(ev) {
+    if (ev.detail.deltaY > 0) {
+      console.log('scrolling down')
+    } else if (ev.detail.deltaY < 0) {
+      console.log('scrolling up')
     }
-  }
+	}
 
-  closeDropdown() {
-    this.showMenu = false;
+	openDropdown(ev) {
+		this.showMenu = !this.showMenu;
+		this.controlMenu = true;
 
-    if (this.controlMenu) {
-      this.showMenu = true;
-      this.controlMenu = false;
-    }
-  }
+		if (!this.showMenu) {
+			ev.stopPropagation();
+			this.controlMenu = false;
+		}
+	}
 
-  slideOpts = {
-    slidesPerView: 1,
-    freeMode: false,
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true
-    }
-  };
+	closeDropdown() {
+		this.showMenu = false;
 
-  onAuthChange() {
-    firebase.auth().onAuthStateChanged(res => {
-      if (res) {
-        this.userService.getUser(res.uid).then(doc => {
-          this.user = doc.data();
-          this.user.id = doc.id;
-        });
-      }
-    });
-  }
+		if (this.controlMenu) {
+			this.showMenu = true;
+			this.controlMenu = false;
+		}
+	}
 
-  getRecipes() {
-    this.recipeService.getAllRecipes().then(result => {
-      result.docs.forEach(doc => {
-        let recipe = doc.data();
-        recipe.id = doc.id;
-        this.recipeService.getRecipeImageUrl(recipe.path).then(url => {
-          recipe.imageUrl = url;
-          this.allRecipes.push(recipe);
-        });
-      });
-    });
-  }
+	onAuthChange() {
+		firebase.auth().onAuthStateChanged(res => {
+			if (res) {
+				this.userService.getUser(res.uid).then(doc => {
+					this.user = doc.data();
+					this.user.id = doc.id;
+				});
+			}
+		});
+	}
 
-  goToUserPage() {
-    this.router.navigate(["/user"], { queryParams: this.user });
-  }
+	getRecipes() {
+		this.recipeService.getAllRecipes().then(result => {
+			result.docs.forEach(doc => {
+				const recipe = doc.data();
+				recipe.id = doc.id;
+				this.recipeService.getRecipeImageUrl(recipe.path).then(url => {
+					recipe.imageUrl = url;
+					this.allRecipes.push(recipe);
+				});
+			});
+		});
+	}
 
-  filterRecipes(type) {
-    console.log(type + "recipes");
-  }
+	goToUserPage() {
+		this.router.navigate(['/user'], { queryParams: this.user });
+	}
+
+	filterRecipes(type) {
+		console.log(type + 'recipes');
+	}
 }
