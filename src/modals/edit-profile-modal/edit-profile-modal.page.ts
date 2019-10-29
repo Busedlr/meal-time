18aec159321d4df0d54c825c81933264ecfd48b2
nicoLepaj <nicolas.lepajolec@gmail.com>
2019-10-29 13:01:17 +0100

    user edit modal -- merge select images functions

--- src/modals/edit-profile-modal/edit-profile-modal.page.ts
+++ src/modals/edit-profile-modal/edit-profile-modal.page.ts
@@ -1,98 +1,91 @@
-import { Component, OnInit } from "@angular/core";
-import { NavParams, ModalController } from "@ionic/angular";
-import * as firebase from "firebase/app";
-import { UserDataService } from "src/services/user-data.service";
+import { Component, OnInit } from '@angular/core';
+import { NavParams, ModalController } from '@ionic/angular';
+import * as firebase from 'firebase/app';
+import { UserDataService } from 'src/services/user-data.service';
 
 @Component({
-  selector: "app-edit-profile-modal",
-  templateUrl: "./edit-profile-modal.page.html",
-  styleUrls: ["./edit-profile-modal.page.scss"]
+	selector: 'app-edit-profile-modal',
+	templateUrl: './edit-profile-modal.page.html',
+	styleUrls: ['./edit-profile-modal.page.scss']
 })
 export class EditProfileModalPage implements OnInit {
-  storageRef: any;
-  user: any;
-  profileImageToSave: any = null;
-  coverImageToSave: any = null;
-  imageToSave: any = null;
-  image: any;
-  coverImage: any;
-  profileImage: any;
+	storageRef: any;
+	user: any;
+	profileImageToSave: any = null;
+	coverImageToSave: any = null;
+	imageToSave: any = null;
+	image: any;
+	coverImage: any;
+	profileImage: any;
 
-  constructor(
-    public navParams: NavParams,
-    public modalController: ModalController,
-    public userService: UserDataService
-  ) {}
+	constructor(
+		public navParams: NavParams,
+		public modalController: ModalController,
+		public userService: UserDataService
+	) {}
 
-  ngOnInit() {
-    this.coverImage = document.getElementById("coverImage") as HTMLImageElement;
-    this.coverImage.src = this.user.coverImageUrl;
-    this.profileImage = document.getElementById(
-      "profileImage"
-    ) as HTMLImageElement;
-    this.profileImage.src = this.user.profileImageUrl;
-  }
+	ngOnInit() {
+		this.coverImage = document.getElementById('coverImage') as HTMLImageElement;
+		this.coverImage.src = this.user.coverImageUrl;
+		this.profileImage = document.getElementById(
+			'profileImage'
+		) as HTMLImageElement;
+		this.profileImage.src = this.user.profileImageUrl;
+	}
 
-  getProfileImage() {
-    this.userService.getProfileImage(this.user.id).then(imageUrl => {
-      this.user.profileImageUrl = imageUrl;
-    });
-  }
+	getProfileImage() {
+		this.userService.getProfileImage(this.user.id).then(imageUrl => {
+			this.user.profileImageUrl = imageUrl;
+		});
+	}
 
-  getCoverImage() {
-    this.userService.getCoverImage(this.user.id).then(coverUrl => {
-      this.user.coverImageUrl = coverUrl;
-    });
-  }
+	getCoverImage() {
+		this.userService.getCoverImage(this.user.id).then(coverUrl => {
+			this.user.coverImageUrl = coverUrl;
+		});
+	}
 
-  resetInput(inputId) {
-    const fileInput = document.getElementById(inputId) as HTMLInputElement;
-    fileInput.value = "";
-  }
+	resetInput(inputId) {
+		const fileInput = document.getElementById(inputId) as HTMLInputElement;
+		fileInput.value = '';
+	}
 
-  selectCoverImage(event) {
-    const file = event.srcElement.files[0];
-    if (
-      file &&
-      (file.type === "image/jpeg" || file.type === "image/png") &&
-      file.size <= 5e6
-    ) {
-      this.coverImageToSave = file;
-      this.coverImage.src = URL.createObjectURL(file);
-    }
-  }
+	selectImage(event, type) {
+		const file = event.srcElement.files[0];
+		if (
+			file &&
+			(file.type === 'image/jpeg' || file.type === 'image/png') &&
+			file.size <= 5e6
+		) {
+			if (type === 'profile') {          
+				this.profileImageToSave = file;
+				this.profileImage.src = URL.createObjectURL(file);
+			} else if (type === 'cover') {
+				this.coverImageToSave = file;
+				this.coverImage.src = URL.createObjectURL(file);
+			}
+		}
+	}
 
-  selectProfileImage(event) {
-    const file = event.srcElement.files[0];
-    if (
-      file &&
-      (file.type === "image/jpeg" || file.type === "image/png") &&
-      file.size <= 5e6
-    ) {
-      this.profileImageToSave = file;
-      this.profileImage.src = URL.createObjectURL(file);
-    }
-  }
+	saveChanges() {
+		if (this.profileImageToSave) {
+			const path = 'user_images/' + this.user.id;
+			this.userService
+				.addProfileImage(path, this.profileImageToSave)
+				.then(() => {
+					this.getProfileImage();
+				});
+		}
+		if (this.coverImageToSave) {
+			const path = 'cover_images/' + this.user.id;
+			this.userService.addCoverImage(path, this.coverImageToSave).then(() => {
+				this.getCoverImage();
+			});
+		}
+		this.closeModal();
+	}
 
-  saveChanges() {
-    if (this.profileImageToSave) {
-      const path = "user_images/" + this.user.id;
-      this.userService
-        .addProfileImage(path, this.profileImageToSave)
-        .then(() => {
-          this.getProfileImage();
-        });
-    }
-    if (this.coverImageToSave) {
-      const path = "cover_images/" + this.user.id;
-      this.userService.addCoverImage(path, this.coverImageToSave).then(() => {
-        this.getCoverImage();
-      });
-    }
-    this.closeModal();
-  }
-
- /*  saveProfileImage() {
+	/*  saveProfileImage() {
     if (this.profileImageToSave) {
       const path = "user_images/" + this.user.id;
       this.userService
@@ -112,7 +105,7 @@ export class EditProfileModalPage implements OnInit {
     }
   } */
 
-  closeModal() {
-    this.modalController.dismiss();
-  }
+	closeModal() {
+		this.modalController.dismiss();
+	}
 }
